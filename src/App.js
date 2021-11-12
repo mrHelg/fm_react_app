2c83ea8c2a63338d9e696755358d208cc554f04f
Олег <osumsky@gmail.com>
2021-11-12 10:09:15 +0200

    Add ciao component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Ciao from './components/Ciao';
 
 function App() {
+  const user = {
+    fname: 'Elon',
+    sname: 'Musk',
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Ciao name={`${user.fname} ${user.sname}`} />
+      <Ciao name="Tim Lee" />
+      <Ciao name="Oleg Sumsky" />
+    </>
   );
 }
 
