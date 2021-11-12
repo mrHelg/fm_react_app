23552f02330d51f445588a56566119a523468b1b
Олег <osumsky@gmail.com>
2021-11-12 12:05:10 +0200

    Add sort users by Alpha

--- src/App.js
+++ src/App.js
@@ -1,20 +1,92 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { Component } from 'react';
 import Ciao from './components/Ciao';
-
-function App() {
-  const user = {
-    fname: 'Elon',
-    sname: 'Musk',
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDirection: true,
+      isAlphabet: true,
+      users: [
+        {
+          id: 1,
+          fname: 'Elon',
+        },
+        {
+          id: 2,
+          fname: 'Elen',
+        },
+        {
+          id: 3,
+          fname: 'Tim',
+        },
+        {
+          id: 4,
+          fname: 'Bob',
+        },
+        {
+          id: 5,
+          fname: 'Sasha',
+        },
+        {
+          id: 6,
+          fname: 'Rob',
+        },
+        {
+          id: 7,
+          fname: 'Alex',
+        },
+        {
+          id: 8,
+          fname: 'Rex',
+        },
+      ],
+    };
+  }
+  sortUsers = () => {
+    const { users, isDirection } = this.state;
+    const sortUsers = JSON.parse(JSON.stringify(users));
+    //const sortUsers = [...users];
+    sortUsers.sort((prev, next) =>
+      isDirection ? next.id - prev.id : prev.id - next.id
+    );
+    this.setState({
+      isDirection: !isDirection,
+      users: sortUsers,
+    });
   };
-  return (
-    <>
-      <Ciao name={`${user.fname} ${user.sname}`} />
-      <Ciao name="Tim Lee" />
-      <Ciao name="Oleg Sumsky" />
-    </>
-  );
+  sortUsersAlpha = () => {
+    const { users, isAlphabet } = this.state;
+    const sortUsers = JSON.parse(JSON.stringify(users));
+    sortUsers.sort((prev, next) => {
+      
+      return isAlphabet ? next.fname > prev.fname : prev.fname < next.fname;
+    });
+    this.setState({
+      isAlphabet: !isAlphabet,
+      users: sortUsers,
+    });
+  };
+  render() {
+    const { users, isDirection, isAlphabet } = this.state;
+    return (
+      <>
+        <p>
+          <button onClick={this.sortUsers}>
+            SORT BY NUMBER {isDirection ? 'straight ' : 'reverse '}
+          </button>
+          <button onClick={this.sortUsersAlpha}>
+            SORT BY NAME {isAlphabet ? 'straight ' : 'reverse '}
+          </button>
+        </p>
+        <div>
+          {users.map(({ id, fname }) => (
+            <Ciao key={id} id={id} name={fname} />
+          ))}
+        </div>
+      </>
+    );
+  }
 }
-
 export default App;
--- src/components/Ciao/index.js
+++ src/components/Ciao/index.js
@@ -1,29 +1,27 @@
-import { Component } from 'react';
-
-class Ciao extends Component {
-  constructor(props) {
+import {Component} from 'react';
+class Ciao extends Component{
+  constructor(props){
     super(props);
     this.state = {
-      isHi: true,
-    };
+      isHi:true
+    }
   }
-  handlerBtn = () => {
-    const { isHi } = this.state;
-    this.setState({ isHi: !isHi });
-  };
-
-  render() {
-    const { name } = this.props;
-    const { isHi } = this.state;
-    return (
-      <>
-        <h2>
-          {isHi ? 'Hello' : 'Bye'}, {name}!
-        </h2>
+  handlerBtn = ()=>{
+    const {isHi} = this.state;
+    this.setState({
+      isHi:!isHi
+    })
+  }
+  render(){
+    const {name, id} = this.props;
+    const {isHi} = this.state;
+    if(isHi){
+      return <>
+        <h2>{id}) Hi, {name}!</h2>
         <button onClick={this.handlerBtn}>Ciao</button>
-      </>
-    );
+        </>;
+    }
+    return <h2>Bye, {name}!</h2>;
   }
 }
-
 export default Ciao;
